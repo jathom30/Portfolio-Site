13473411ef94954c97b3ee01001c47b0185c37bc
Jeff Thomas <jathom30@gmail.com>
2018-01-29 09:02:55 -0500

    updated css to scss and started filling in About page

--- src/components/About.js
+++ src/components/About.js
@@ -5,17 +5,28 @@ const About = () => {
     <div className="about main" id="about">
       <h1>About</h1>
 
-      <div>
+      <div className="info">
         <h2>brief bio</h2>
-        <p>a brief bio describing my hobbies and interests.</p>
+        <p>Jeff Thomas is an unashamed Junkrat main. He graduated from the University of Louisville with a BA in Jazz Studies with an emphasis in Guitar in 2011. Since then he has enjoyed playing in Louisville based jazz band, Small Time Napoleon. With STN he has played hundreds of shows across several states to audiences ranging from intimate private shows to large outdoor festivals.</p>
+        <p>He currenty works as a photo editor and graphic designer for a local photo. Doing so has allowed Jeff to grow as a graphic designer by building custom yearbooks and senior composites for several Louisville area schools.</p>
+        <p>During the past two to three years, Jeff has had an increased interest in graphic design and coding. This eventually led to an enrollment in Code Louisville in their Front-End Web Development and Python courses. Since graduating, Jeff has continued to self learn new languages and libraries in an attempt to stay current with the day's tech.</p>
       </div>
 
-      <div>
+      <div className="info">
         <h2>education/experience</h2>
-        <p>some words on Code Louisville and related real world experience. as well as what coding languages/technologies i'm comfortable working in.</p>
+        <ul>
+          <li>University of Louisville - BA Jazz Studies - 2011</li>
+          <li>Small Time Napoleon named "Jazz/Avant Garde Artist of the Year" - Louisville Music Awards - 2015</li>
+          <li>Code Louisville - Front-End Web Development - 2017</li>
+          <li>Code Louisville - Python - 2017</li>
+          <li>Small Time Napoleon named "Jazz/Avant Garde Artist of the Year" - Louisville Music Awards - 2017</li>
+          <li>Signature Studio Yearbook Website Deployed - 2017</li>
+          <li>Small Time Napoleon Website Deployed - 2018</li>
+          <li>Angel Website Deployed - 2018</li>
+        </ul>
       </div>
 
-      <div>
+      <div className="info">
         <h2>what i'm looking for</h2>
         <p>what i'm looking for in an employeer or what projects i might like to take on.</p>
       </div>
--- src/components/portfolio/Web.js
+++ src/components/portfolio/Web.js
@@ -35,7 +35,7 @@ export default class Web extends Component {
         <div className="info">
           <h2>Websites</h2>
           <p>I have a strong interest in front-end web. After completing Code Louisville's Front-End Web Development track as well as their Python track, I've gone on to self-learn SASS, React, and updated my JavaScript learning with CS6.</p>
-          <p>As a result, I am proficient in HTML, CSS/SCSS/SASS, JavaScript, JQuery, React, and Python/Django. My projects have been used for Jeffersontown Police's Angel Program, Signature Studio, Small Time Napoleon, and other person projects.</p>
+          <p>As a result, I am proficient in HTML, CSS/SCSS, JavaScript, JQuery, React, and Python/Django. My projects have been used for Jeffersontown Police's Angel Program, Signature Studio, Small Time Napoleon, and other personal projects.</p>
         </div>
 
         <div className="links">
