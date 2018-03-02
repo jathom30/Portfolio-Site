b1235c0b870207cf5101c99383b1a22a8e0edbb7
Jeff Thomas <jathom30@gmail.com>
2018-03-02 14:55:12 -0500

    styling updates to home and added click func to beetle on home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { TimelineMax, Elastic, Power1, Sine } from 'gsap';
+import { TimelineMax, Elastic, Sine } from 'gsap';
 
 import BeetlePieces from '../assets/BeetlePieces';
 import SocialMedia from './SocialMedia';
@@ -7,53 +7,38 @@ import SocialMedia from './SocialMedia';
 export default class Home extends Component {
 
   animateTheBeetle() {
+    const legRepeats = 5;
+    const legMovementTime = .25;
     const walk = new TimelineMax();
     walk
-      .add('take-off', 5)
-      .add('wings-beat', 4.5)
-      .add('wings-show', 3)
-    walk
       //beetle walks in
-      .from("#beetle", 1.75, {y: '40%', autoAlpha:0, ease: Power1.easeOut})
-      //shell closes as in just landed
-      .to("#left-shell", .25, {rotation:'-24', transformOrigin:'right', ease:Sine.easeInOut, repeat: 0}, '0')
-      .to("#right-shell", .25, {rotation:'24', transformOrigin:'left', ease:Sine.easeInOut, repeat: 0}, '.1')
+      .from("#beetle", 1.7, {y: '80px', autoAlpha:0, ease: Sine.easeOut})
+      //start with shell closed
+      .set("#left-shell",{rotation:'-20', transformOrigin:'right', ease:Sine.easeInOut, repeat: 0}, '0')
+      .set("#right-shell",{rotation:'20', transformOrigin:'left', ease:Sine.easeInOut, repeat: 0}, '0')
       //legs walk
-      .from("#left-front-leg", .5, {rotation: '-20', transformOrigin: '75px bottom', ease:Sine.easeInOut, repeat:2, yoyo:true}, '0')
-      .from("#left-mid-leg", .5, {rotation: '-20', transformOrigin: 'right 35px', ease:Sine.easeInOut, repeat:2, yoyo:true}, '.2')
-      .to("#left-rear-leg", .5, {rotation: '30', transformOrigin: 'right 35px', ease:Sine.easeInOut, repeat:2, yoyo:true}, '.4')
+      .to("#left-front-leg", legMovementTime, {rotation: '-20', transformOrigin: '75px bottom', ease:Sine.easeInOut, repeat:legRepeats, yoyo:true}, '0')
+      .to("#left-mid-leg", legMovementTime, {rotation: '-20', transformOrigin: 'right 35px', ease:Sine.easeInOut, repeat:legRepeats, yoyo:true}, '.1')
+      .to("#left-rear-leg", legMovementTime, {rotation: '30', transformOrigin: 'right top', ease:Sine.easeInOut, repeat:legRepeats, yoyo:true}, '.1')
       
-      .to("#right-front-leg", .5, {rotation: '20', transformOrigin: 'left bottom', ease:Sine.easeInOut, repeat:2, yoyo:true}, '.1')
-      .from("#right-mid-leg", .5, {rotation: '20', transformOrigin: 'left 35px', ease:Sine.easeInOut, repeat:2, yoyo:true}, '.4')
-      .from("#right-rear-leg", .5, {rotation: '-30', transformOrigin: 'left 35px', ease:Sine.easeInOut, repeat:1, yoyo:true}, '.5')
+      .to("#right-front-leg", legMovementTime, {rotation: '20', transformOrigin: 'left bottom', ease:Sine.easeInOut, repeat:legRepeats, yoyo:true}, '.2')
+      .to("#right-mid-leg", legMovementTime, {rotation: '20', transformOrigin: 'left 35px', ease:Sine.easeInOut, repeat:legRepeats, yoyo:true}, '.3')
+      .to("#right-rear-leg", legMovementTime, {rotation: '-30', transformOrigin: 'left top', ease:Sine.easeInOut, repeat:legRepeats, yoyo:true}, '.3')
       //shell opens for wings
-      .to("#left-shell", 2, {rotation:'0', transformOrigin:'right', ease: Elastic.easeOut.config(2, .75), repeat:0}, '1.5')
-      .to("#right-shell", 2, {rotation:'0', transformOrigin:'left', ease: Elastic.easeOut.config(2, .75), repeat:0}, '1.5')
-      .to('#left-antenna', 2, {rotation: '20', transformOrigin:'right', ease: Power1.easeInOut, repeat: 2, yoyo: true}, '1.5')
-      .to('#right-antenna', 2, {rotation: '-20', transformOrigin:'left', ease: Power1.easeInOut, repeat: 2, yoyo: true}, '1.5')
-      //wings show
-      .fromTo("#left-wing", 3, {rotation: '-40', transformOrigin:'right bottom', autoAlpha: 0}, {rotation: '0', autoAlpha: .75, ease: Elastic.easeOut.config(1, .5)}, 'wings-show')
-      .fromTo("#right-wing", 3, {rotation: '40', transformOrigin:'left bottom', autoAlpha: 0}, {rotation: '0', autoAlpha: .75, ease: Elastic.easeOut.config(1, .5)}, 'wings-show')
-      //legs retract while flying
-      .to("#left-front-leg", .5, {rotation: '-50', transformOrigin: '75px bottom', ease: Power1.easeInOut}, 'take-off')
-      .to("#left-mid-leg", .5, {rotation: '-50', transformOrigin: 'right 35px', ease: Power1.easeInOut}, 'take-off')
-      .to("#left-rear-leg", .5, {rotation: '-10', transformOrigin: 'right 35px', ease: Power1.easeInOut}, 'take-off')
-      
-      .to("#right-front-leg", .5, {rotation: '50', transformOrigin: 'left bottom', ease: Power1.easeInOut}, 'take-off')
-      .to("#right-mid-leg", .5, {rotation: '50', transformOrigin: 'left 35px', ease: Power1.easeInOut}, 'take-off')
-      .to("#right-rear-leg", .5, {rotation: '10', transformOrigin: 'left 35px', ease: Power1.easeInOut}, 'take-off')
-      //shell closes slightly midair
-      .to("#left-shell", .5, {rotation: '-20', transformOrigin: 'right', ease: Power1.easeInOut}, 'take-off')
-      .to("#right-shell", .5, {rotation: '20', transformOrigin: 'left', ease: Power1.easeInOut}, 'take-off')
-      //wings beat for flight
-      .to("#left-wing", .1, {rotation: '-10', transformOrigin: 'right bottom', repeat: 20}, 'wings-beat')
-      .to("#right-wing", .1, {rotation: '10', transformOrigin: 'left bottom', repeat: 20}, 'wings-beat')
-      //beetle flies up
-      .to("#beetle", 1, {y: '-100%', autoAlpha: 0, scale: 2, ease: Power1.easeInOut}, 'take-off')
+      .to("#left-shell", 2, {rotation:'-15', transformOrigin:'right', ease: Elastic.easeOut.config(2, .75), repeat:0}, '1.5')
+      .to("#right-shell", 2, {rotation:'15', transformOrigin:'left', ease: Elastic.easeOut.config(2, .75), repeat:0}, '1.6')
+      //antenna movements
+      .to('#left-antenna', .1, {rotation: '10', transformOrigin:'right', ease: Sine.easeInOut, repeat: 1, yoyo: true}, '.5')
+      .to('#left-antenna', .1, {rotation: '10', transformOrigin:'right', ease: Sine.easeInOut, repeat: 1, yoyo: true}, '5')
+      .to('#left-antenna', .1, {rotation: '10', transformOrigin:'right', ease: Sine.easeInOut, repeat: 1, yoyo: true}, '10.9')
+
+      .to('#right-antenna', .1, {rotation: '-10', transformOrigin:'left', ease: Sine.easeInOut, repeat: 1, yoyo: true}, '.4')
+      .to('#right-antenna', .1, {rotation: '-10', transformOrigin:'left', ease: Sine.easeInOut, repeat: 1, yoyo: true}, '5')  
+      .to('#right-antenna', .1, {rotation: '-10', transformOrigin:'left', ease: Sine.easeInOut, repeat: 1, yoyo: true}, '11')
   }
 
   componentDidMount() {
-    // this.animateTheBeetle();
+    this.animateTheBeetle();
   }
   
   render() {
