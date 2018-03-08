8a56779565923d00dff605c6c9867f3315b9a3e8
Jeff Thomas <jathom30@gmail.com>
2018-03-08 12:23:47 -0500

    updated animations to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react';
-import { TimelineMax, Power1 } from 'gsap';
 
-// import BeetleIcon from '../assets/BeetleIcon';
+import BeetleIcon from '../assets/BeetleIcon';
 import Hamburger from '../assets/Hamburger';
+import { TimelineLite } from 'gsap';
 
 export default class Header extends Component {
-  // componentDidMount() {
-  //   const tl = new TimelineMax(),
-  //         header = document.getElementById('header'),
-  //         logo = document.getElementById('logo'),
-  //         hamburger = document.getElementById('hamburger');
 
-  //   tl
-  //     .from(header, 2, {y: '-100%', autoAlpha: 0, ease: Power1.easeOut})
-  //     .from(logo, 1, {autoAlpha: 0, ease: Power1.easeInOut}, 1)
-  //     .from(hamburger, 1, {autoAlpha: 0, ease: Power1.easeInOut}, 1);
-  // }
+  crossBuns() {
+    const tl = new TimelineLite();
+    tl
+      .to("#top", .5, {y: '30px'})
+      .to("#bottom", .5, {y: '-30px'}, 0)
+      .to("#top", .3, {rotation: -45, transformOrigin: 'center'}, .3)
+      .to("#bottom", .3, {rotation: 45, transformOrigin: 'center'}, .3)
+      .to("#middle", .5, {autoAlpha: 0}, .2)
+  }
+  uncrossBuns() {
+    const tl = new TimelineLite();
+    tl
+      .to("#top", .3, {rotation: 0})
+      .to("#bottom", .3, {rotation: 0}, 0)
+      .to("#top", .3, {y: 0}, .3)
+      .to("#bottom", .3, {y: 0}, .3)
+      .to("#middle", .5, {autoAlpha: 1}, .2)
+  }
 
 
   render () {
-
     const { nav, showHideNav, hideNav } = this.props;
+
+    if (nav) {
+      this.crossBuns();
+    } else {
+      this.uncrossBuns();
+    }
+
 
     return (
       <div id="header" className={nav ? "header full" : "header"} onClick={showHideNav}>
@@ -34,6 +48,7 @@ export default class Header extends Component {
         </div>
     
         <div className="header-links" style={!nav ? {display: 'none'} : {} } >
+          <a href="#home" onClick={hideNav}><BeetleIcon /></a>
           <a href="#portfolio" onClick={hideNav}>portfolio</a>
           <a href="#about" onClick={hideNav}>about</a>
           <a href="#contact" onClick={hideNav}>contact</a>
